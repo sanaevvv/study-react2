f2dfc86934e2e6eba16b2bf56996e1c8982e6846
sanaevvv <sanaevvv@gmail.com>
2021-08-29 09:33:07 +0900

    カウンターを設置してuseCounterでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,19 +3,17 @@ import styles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer';
 import { Main } from '../components/Main';
 import { Header } from '../components/Header';
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = (e) => {
+    setCount((prevState) => prevState + 1);
+  };
 
   useEffect(() => {
-    console.log('マウント時');
     document.body.style.backgroundColor = 'lightblue';
     return () => {
-      console.log('アンマウント時');
       document.body.style.backgroundColor = '';
     };
   }, []);
@@ -28,9 +26,8 @@ export default function Home() {
       </Head>
 
       <Header />
-      <a href="" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 

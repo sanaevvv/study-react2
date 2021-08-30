e07bb4bc7cc9bcdfca7d0ce87f65b877a343acba
sanaevvv <sanaevvv@gmail.com>
2021-08-30 16:21:57 +0900

    useMemo

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,8 +1,12 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 export const useCounter = () => {
   const [count, setCount] = useState(1);
   const [isShow, setIsShow] = useState(true);
+
+  const doubleCount = useMemo(() => {
+    return count * 2;
+  }, [count]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -13,5 +17,5 @@ export const useCounter = () => {
     setIsShow((prevState) => !prevState);
   }, []);
 
-  return { count, isShow, handleClick, handleDisplay };
+  return { count, isShow, doubleCount, handleClick, handleDisplay };
 };

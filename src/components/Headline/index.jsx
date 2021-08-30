d2dfdfc7b70f700cdb7390619083420767283156
sanaevvv <sanaevvv@gmail.com>
2021-08-30 14:52:53 +0900

    コンポーネント間のリフトアップおよびsetItemsの引数変更

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,11 +1,13 @@
 import styles from 'src/components/Headline/Headline.module.css';
 
-export function Headline({ page, children }) {
+export function Headline({ page, children, handleReduce }) {
   return (
     <>
       <h1 className={styles.title}>{page} Page</h1>
 
-      <p className={styles.description}>Get started by edit {children}</p>
+      <p className={styles.description}>アイテムの数は{children}個です</p>
+
+      <button onClick={handleReduce}>減らす</button>
     </>
   );
 }
--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,37 +1,6 @@
-import { useCallback, useState } from 'react';
 import styles from 'src/components/Links/Links.module.css';
 
-const ITEMS = [
-  {
-    href: 'https://nextjs.org/docs',
-    title: 'Documentation →',
-    body: 'Find in-depth information about Next.js features and API.',
-  },
-  {
-    href: 'https://nextjs.org/learn',
-    title: 'Learn →',
-    body: 'Learn about Next.js in an interactive course with quizzes!',
-  },
-  {
-    href: 'https://github.com/vercel/next.js/tree/master/examples',
-    title: 'Documentation →',
-    body: 'Discover and deploy boilerplate example Next.js projects.',
-  },
-  {
-    href: 'https://nextjs.org/docshttps://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-    title: 'Deploy →',
-    body: 'Instantly deploy your Next.js site to a public URL with Vercel.',
-  },
-];
-
-export function Links() {
-  const [items, setItems] = useState(ITEMS);
-  const handleReduce = useCallback(() => {
-    setItems((items) => {
-      items.slice(0, items.length - 1);
-    });
-  }, []);
-
+export function Links({ items, handleReduce }) {
   return (
     <div className={styles.grid}>
       {items.map((item) => {

0ff83d283853f1bc4d1c6ef49fa26834e219fe6b
sanaevvv <sanaevvv@gmail.com>
2021-09-02 16:34:08 +0900

    ファイルシステムルーティング

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,7 +1,10 @@
-import { usePosts } from "src/hooks/usePosts";
+import Link from 'next/link';
+import { usePosts } from 'src/hooks/usePosts';
 
 export const Posts = () => {
   const { data, error, isLoading, isEmpty } = usePosts();
+
+  console.log({ data, error });
 
   if (isLoading) {
     return <div>ローディング中</div>;
@@ -18,7 +21,13 @@ export const Posts = () => {
   return (
     <ol>
       {data.map((post) => {
-        return <li key={post.id}>{post.title}</li>;
+        return <li key={post.id}>
+          <Link href={`/post/${post.id}`}>
+            <a>
+              {post.title}
+              </a>
+            </Link>
+        </li>;
       })}
     </ol>
   );
